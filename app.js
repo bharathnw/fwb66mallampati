3ec9acaeafba28609c9864edcddc0cef3c0bf1a0
bharathnw <msreebharath21@gmail.com>
2021-11-03 17:13:20 -0500

    changes done related to query params

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var georgiaRouter = require('./routes/georgia');
+var addModsRouter = require('./routes/addmods');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/georgia', georgiaRouter);
+app.use('/addmods', addModsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
