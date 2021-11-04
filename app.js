ccb9db6b4140400a9f47082bd122ae006e3d2bdb
bharathnw <msreebharath21@gmail.com>
2021-11-03 20:58:48 -0500

    changes related to selector

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var georgiaRouter = require('./routes/georgia');
 var addModsRouter = require('./routes/addmods');
 var usersRouter = require('./routes/users');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/georgia', georgiaRouter);
 app.use('/addmods', addModsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/addmods.js
+++ routes/addmods.js
@@ -1,13 +1,17 @@
+const e = require('express');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     let query = req.query;
-    console.log(`rows ${query.rows}`)
-    console.log(`cols ${query.cols}`)
-    res.render('index', { title: 'Add Mods', query: query });
-
+    if (!Number.isInteger(parseInt(query.rows)) || !Number.isInteger(parseInt(query.cols))) {
+        res.send('Please pass valid query params to execute');
+    } else if (parseInt(query.rows) == 0 || parseInt(query.cols) == 0) {
+        res.send('0 cannot be used to generate a table');
+    } else {
+        res.render('addmods', { title: 'Add Mods', query: query });
+    }
 });
 
 module.exports = router;
